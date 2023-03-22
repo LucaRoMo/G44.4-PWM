d36417567c47d56ef9fb1da8c16f07c28b949df3
pablo <pablosanmel05@gmail.com>
2023-03-22 09:19:31 +0000

    user_list arreglado

---
+++ JS/js_carousel.js
@@ -0,0 +1,29 @@
+const carousel = document.querySelector('.carousel');
+const slides = carousel.querySelector('.slides');
+const prevButton = carousel.querySelector('.prev');
+const nextButton = carousel.querySelector('.next');
+const slideWidth = carousel.clientWidth;
+let slideIndex = 0;
+
+function slideTo(index) {
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+function prevSlide() {
+    slideIndex--;
+    if (slideIndex < 0) {
+        slideIndex = slides.children.length - 1;
+    }
+    slideTo(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex >= slides.children.length) {
+        slideIndex = 0;
+    }
+    slideTo(slideIndex);
+}
+
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
