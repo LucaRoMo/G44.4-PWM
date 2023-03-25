63a93bda2f5be92269f9f1b0eeefc140b2924b41
LucaRodriguez <luca.rodriguez101@alu.ulpgc.es>
2023-03-25 13:45:45 +0000

    Done dynamic load for user profile page. Added template for a lot of things. Added a lot of things

--- JS/dynamic_Load.js
+++ JS/dynamic_Load.js
@@ -1,7 +1,7 @@
 fetch("../JSON_Files/users.json")
     .then(response => response.json())
     .then(data => {
-        const usuario = data.usuarios.find(u => u.id === 1);
+        const usuario = data.find(u => u.id === 1);
         console.log(usuario.nombre);
         document.getElementById('elongated_card_user_text').textContent = usuario.nombre;
     })
