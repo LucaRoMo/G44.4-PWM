b53f942ae01fc875db56fd387ae22ef3f39fa6c5
Luca <luca.rodriguez101@alu.ulpgc.es>
2023-04-24 12:10:23 +0100

    Added angular routing to project and miAcademia as a test

--- sprint3/src/app/app-routing.module.ts
+++ sprint3/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MiAcademiaComponent} from "./mi-academia/mi-academia.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'miAcademia',component: MiAcademiaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
