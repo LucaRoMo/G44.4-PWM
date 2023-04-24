fb7ac1cb27a66de758f78973cdd94ab536e54a32
pablo <pablosanmel05@gmail.com>
2023-04-24 11:07:15 +0100

    Noticias completo

---
+++ sprint3/src/app/noticias-principal/noticias-principal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noticias-principal',
+  templateUrl: './noticias-principal.component.html',
+  styleUrls: ['./noticias-principal.component.css']
+})
+export class NoticiasPrincipalComponent {
+
+}
