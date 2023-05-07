981983241370bf559935318cc27bdb8dd137b915
CarlosMontoyaHidalgo <carlosmh561@gmail.com>
2023-05-07 17:35:30 +0100

    guardado provisional

--- sprint2/JS/dynamic_Load.js
+++
@@ -1,8 +0,0 @@
-fetch("../JSON_Files/users.json")
-    .then(response => response.json())
-    .then(data => {
-        const usuario = data.find(u => u.id === 1);
-        console.log(usuario.nombre);
-        document.getElementById('elongated_card_user_text').textContent = usuario.nombre;
-    })
-    .catch(error => console.error(error));
